840f76c218dc5556f2bf1561bda2c23f6d46dc16
Dominik Stodolny <dominik@stodolny.org>
2021-04-22 08:57:15 +0100

    parse uri

--- labs/7_proxy_lab/proxy.c
+++ labs/7_proxy_lab/proxy.c
@@ -9,8 +9,10 @@
 
 void doit(int fd);
 int is_valid_method(char *method);
-int is_valid_uri(char *uri);
 int is_valid_version(char *version);
+int is_absolute_uri(char *uri);
+int is_absolute_path(char *uri);
+int parse_uri(char *uri, char *host, char *path);
 
 /* static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n"; */
 
@@ -18,20 +20,38 @@ void
 doit(int fd)
 {
         char buf[MAXLINE], method[MAXLINE], uri[MAXLINE],
-                version[MAXLINE];
+                host[MAXLINE], path[MAXLINE], version[MAXLINE];
         rio_t rio;
 
+        /* read request */
         Rio_readinitb(&rio, fd);
         if (!Rio_readlineb(&rio, buf, MAXLINE))
-                return;
+                printf("TODO: HANDLE ERROR\n");
         printf("%s", buf);
         sscanf(buf, "%s %s %s", method, uri, version);
 
+        /* transform the uri to lowercase */
+        char *urip = uri;
+        for ( ; *urip; urip++)
+                *urip = tolower(*urip);
+
+        /* check if the method and version is valid */
         if (!(is_valid_method(method) &&
-              is_valid_uri(uri) &&
               is_valid_version(version))) {
                 printf("TODO: HANDLE ERROR\n");
         }
+
+        /* parse uri */
+        if (!parse_uri(uri, host, path))
+                printf("TODO: HANDLE ERROR\n");
+
+        printf("host: %s\n", host);
+        printf("path: %s\n", path);
+
+        /* Read headers */
+
+
+
 }
 
 int
@@ -41,24 +61,49 @@ is_valid_method(char *method)
 }
 
 int
-is_valid_uri(char *uri)
-{
-        char *urip = uri;
-
-        for ( ; *urip; urip++)
-                *urip = tolower(*urip);
-
-        return strstr(uri, "http://") == uri ||
-                strstr(uri, "/") == uri;
-}
-
-int
 is_valid_version(char *version)
 {
         return strcasecmp(version, "HTTP/0.9") == 0 ||
                 strcasecmp(version, "HTTP/1.0") == 0 ||
                 strcasecmp(version, "HTTP/1.1") == 0 ||
                 strcasecmp(version, "HTTP/2.0") == 0;
+}
+
+int
+is_absolute_uri(char *uri)
+{
+        return strstr(uri, "http://") == uri;
+}
+
+int
+is_absolute_path(char *uri)
+{
+        return strstr(uri, "/") == uri;
+}
+
+/*
+ * parse_uri - Parses given uri and extracts host and path. If the
+ * result is successful it returns 1. Otherwise it returns 0.
+ */
+int
+parse_uri(char *uri, char *host, char *path)
+{
+        char *ptr;
+
+        if (is_absolute_uri(uri)) {
+                printf("%s\n", uri + strlen("http://"));
+                if ((ptr = strchr(uri+strlen("http://"), '/')) == NULL)
+                        return 0;
+                strcpy(path, ptr);
+                *ptr = '\0';
+                strcpy(host, uri+strlen("http://"));
+                return 1;
+        } else if (is_absolute_path(uri)) {
+                strcpy(path, uri);
+                strcpy(host, "");
+                return 1;
+        }
+        return 0;
 }
 
 
